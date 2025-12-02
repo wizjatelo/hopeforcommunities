624694bb0ceff325d47b15048aa7545d4d9afb26
kloss254 <129971392+kloss254@users.noreply.github.com>
2025-12-02 14:41:03 +0300

    My local changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,6 @@ import About from "./pages/About";
 import GetInvolved from "./pages/GetInvolved";
 import Programs from "./pages/Programs";
 import Impact from "./pages/Impact";
-﻿
-
-Press ctrl i to turn on code suggestions. Press ctrl x to disable code suggestions.
-ctrl
-i
- to turn on code suggestions. Don't show again NEW
-
-Full stack trace AI agent in Performance panel
-The Performance panel now integrates a more capable AI agent that has full access to the stack trace.
-
-AI code completion in Sources and Console
-Code completion powered by AI is now available in the Sources panel and the Console.
-
-Google Developer Program support
-The Google Developer Program, a ready-made subscription and community platform, is now in DevTools, offering a more personalized experience, a badge system, and easier discovery of new features.
-
-One main 'Debug with AI' button
-The primary action bar at the top of DevTools now features the primary "Debug with AI" button which opens the 'AI assistance' panel in a right-hand side bar.
 
 function App() {
   return (
