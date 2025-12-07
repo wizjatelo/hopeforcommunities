3eef91888623e1024cd92d2b722b2cae6754b027
kloss254 <129971392+kloss254@users.noreply.github.com>
2025-12-07 12:26:10 +0300

    Prepare deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Custom domain at root
-  server: {
-    port: 3000,
-    open: true
-  }
-});
+  base: '/hopeforcommunities/', // VERY IMPORTANT
+})
