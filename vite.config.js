66d068ecb80b04696e954076f48add52520d2727
kloss254 <129971392+kloss254@users.noreply.github.com>
2025-12-07 13:41:36 +0300

    fix: updated vite config base path and file structure

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hopeforcommunities/', // VERY IMPORTANT
+  base: '/hopeforcommunities/', // Matches your repo name exactly
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true, // Clears the folder before rebuilding
+  }
 })
