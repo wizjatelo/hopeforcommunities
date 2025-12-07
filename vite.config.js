69c863d320dabbd1672856f4b210df9cea546658
wizjatelo <wisdomarkanyange@gmail.com>
2025-12-07 11:38:08 +0300

    Add CNAME for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changed from '/hopeforcommunities/' to '/'
+  base: '/', // Custom domain at root
   server: {
     port: 3000,
     open: true
